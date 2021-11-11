d65e53cac3b1a6725ba4a10d67e2e582d67705ae
jxxtty <jaehee941106@gmail.com>
2021-11-11 16:42:16 +0900

    아이템 제거하기

--- src/App.js
+++ src/App.js
@@ -71,8 +71,13 @@ class App extends Component{
     this.setState({
       todos: nextTodos
     });
+  }
 
-
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
   }
 
 
@@ -82,14 +87,15 @@ class App extends Component{
       handleChange,
       handleCreate,
       handleKeyPress,
-      handleToggle
+      handleToggle,
+      handleRemove
     } = this;
 
     return (
       <TodoListTemplate form={(
         <Form value={input} onKeyPress={handleKeyPress} onChange={handleChange} onCreate={handleCreate}/>
       )}>
-        <TodoItemList todos={todos} onToggle={handleToggle}/>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
       </TodoListTemplate>
     );
   }
